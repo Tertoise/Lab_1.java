5465924322e00e1757b95f9fa57280b0d349c6cf
Danylo Borysenko <bdadaniil10@gmail.com>
2022-10-09 19:33:21 +0300

    Adding 2 lab

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-
-public class Main{
-    public static void main(String[] args) throws Exception{
-        System.out.println(new Stringcalculator().add("//[***][**][*****]\n1***2**3*****4"));
-    }
-}
