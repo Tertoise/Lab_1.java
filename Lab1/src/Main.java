8b751ccf1c8105860c517fd13c024b97262c759a
Borysenko Danylo <bdadaniil10@gmail.com>
2022-12-26 21:04:00 +0200

    Adding the 3rd lab

--- Lab1/src/Main.java
+++
@@ -1,6 +0,0 @@
-
-public class Main{
-    public static void main(String[] args) throws Exception{
-        System.out.println(new Stringcalculator().add("//[**][****][2][(]\n1******1**1,1\n121999"));
-    }
-}
